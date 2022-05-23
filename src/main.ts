2456c3afd0f0ab254f918977a76193299149ef6a
vista0212 <kbaek1202@gmail.com>
2022-05-24 08:07:53 +0900

    스웨거 세팅 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './AppModule';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('geul API')
+    .setDescription('geul API description')
+    .setVersion('1.0')
+    .addTag('geul-api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
