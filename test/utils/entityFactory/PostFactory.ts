5d2d2cc1bad4ef98e3c7146dc864e5e518bde199
vista0212 <kbaek1202@gmail.com>
2022-06-07 08:34:06 +0900

    post factory 추가

---
+++ test/utils/entityFactory/PostFactory.ts
@@ -0,0 +1,21 @@
+import { Factory, faker } from '@mikro-orm/seeder';
+import { Post } from '@app/entity/post/Post.entity';
+import { PostStatus } from '@app/entity/post/type/PostType';
+
+export class PostFactory extends Factory<Post> {
+  model = Post;
+
+  static createEntity(entity?: Partial<Post>): Post {
+    return Object.assign(new Post(), {
+      title: faker.datatype.string(),
+      body: faker.datatype.string(),
+      status: faker.random.arrayElement(Object.values(PostStatus)),
+      viewCount: 0,
+      ...entity,
+    } as Post);
+  }
+
+  definition(): Partial<Post> {
+    return PostFactory.createEntity();
+  }
+}
