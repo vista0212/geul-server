c5f52d621bc5c3f80a3296b27279d157dca2aa93
vista0212 <kbaek1202@gmail.com>
2022-06-08 08:38:27 +0900

    PostFindRequest 추가

---
+++ src/post/dto/PostFindRequest.ts
@@ -0,0 +1,29 @@
+import { IsArray, IsInt, IsOptional } from 'class-validator';
+import { FilterQuery } from '@mikro-orm/core';
+import { Post } from '@app/entity/post/Post.entity';
+import { PostStatus } from '@app/entity/post/type/PostType';
+import { LocalDateTime } from '@js-joda/core';
+import { SliceRequest } from '@app/web-common/req/SliceRequest';
+
+export class PostFindRequest extends SliceRequest {
+  @IsInt()
+  @IsOptional()
+  lastId?: number;
+
+  @IsArray()
+  @IsOptional()
+  tags: string[] = [];
+
+  toWhereFilter(now: LocalDateTime): FilterQuery<Post> {
+    const where: FilterQuery<Post> = {};
+
+    if (this.lastId) {
+      where.id = { $lt: this.lastId };
+    }
+
+    where.status = PostStatus.PUBLISHED;
+    where.publishedAt = { $lte: now };
+
+    return where;
+  }
+}
