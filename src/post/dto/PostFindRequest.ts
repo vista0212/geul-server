d34f79d43d5fad83a64ca708a09e21adb4c9d3d2
vista0212 <kbaek1202@gmail.com>
2022-08-01 20:01:29 +0900

    작업 기기 변경을 위한 커밋

--- src/post/dto/PostFindRequest.ts
+++ src/post/dto/PostFindRequest.ts
@@ -1,24 +1,39 @@
-import { IsArray, IsInt, IsOptional } from 'class-validator';
+import { IsArray, IsInt, IsOptional, IsString } from 'class-validator';
 import { FilterQuery } from '@mikro-orm/core';
 import { Post } from '@app/entity/post/Post.entity';
 import { PostStatus } from '@app/entity/post/type/PostType';
 import { LocalDateTime } from '@js-joda/core';
 import { SliceRequest } from '@app/web-common/req/SliceRequest';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PostFindRequest extends SliceRequest {
+  @ApiProperty()
   @IsInt()
   @IsOptional()
   lastId?: number;
 
+  @ApiProperty()
   @IsArray()
   @IsOptional()
   tags: string[] = [];
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  keyword: string;
 
   toWhereFilter(now: LocalDateTime): FilterQuery<Post> {
     const where: FilterQuery<Post> = {};
 
     if (this.lastId) {
       where.id = { $lt: this.lastId };
+    }
+
+    if (this.keyword) {
+      where.$or = [
+        { title: { $ilike: `%${this.keyword}%` } },
+        { body: { $ilike: `%${this.keyword}%` } },
+      ];
     }
 
     where.status = PostStatus.PUBLISH;
